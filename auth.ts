5f8c8335ad2f8355f311c9b3976f48f417f0d130
addy <addyyy118@gmail.com>
2025-06-13 21:08:39 +0530

    feat: add user_roles

--- auth.ts
+++ auth.ts
@@ -2,6 +2,7 @@ import NextAuth from "next-auth";
 import authConfig from "./auth.config";
 import { db } from "./lib/db";
 import { PrismaAdapter } from "@auth/prisma-adapter";
+import User from "./data/user";
 
 export const {
   // nested destructuring
@@ -10,6 +11,46 @@ export const {
   signOut,
   auth,
 } = NextAuth({
+  callbacks: {
+    // // don't allow unverified email users to login
+    // async signIn({ user }) {
+    //   if (!user.id) return false;
+    //   const existingUser = await User.getById(user.id);
+
+    //   if (!existingUser || !existingUser.emailVerified) {
+    //     return false;
+    //   }
+
+    //   return true;
+    // },
+
+    async jwt({ token }) {
+      if (!token.sub) return token;
+
+      const existingUser = await User.getById(token.sub);
+      if (!existingUser) return token;
+
+      token.role = existingUser.role;
+      return token;
+    },
+
+    async session({ token, session }) {
+      console.log({
+        sessionToken: token,
+        session,
+      });
+
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role;
+      }
+
+      return session;
+    },
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
