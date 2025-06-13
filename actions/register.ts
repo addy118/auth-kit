fa2a2581639212d2fa8aebf046cb1f058a5ceabb
addy <addyyy118@gmail.com>
2025-06-13 15:54:44 +0530

    feat: setup prisma

--- actions/register.ts
+++ actions/register.ts
@@ -1,8 +1,10 @@
 "use server";
 
-import {  RegiSchema } from "@/schemas";
+import bcrypt from "bcrypt";
+import { RegiSchema } from "@/schemas";
 import * as z from "zod";
-
+import { db } from "@/lib/db";
+import User from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegiSchema>) => {
   console.log(values);
@@ -10,5 +12,23 @@ export const register = async (values: z.infer<typeof RegiSchema>) => {
 
   if (!validatedFields) return { error: "Invalid Fields" };
 
-  return { success: "Email sent!" };
+  const { name, email, password } = validatedFields.data!;
+  const hashedPass = await bcrypt.hash(password, 10);
+
+  const existingUser = await User.getByEmail(email);
+
+  if (existingUser) return { error: "Email already in use!" };
+  console.log(name, email, hashedPass);
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPass,
+    },
+  });
+
+  // TODO: send verfication token email
+
+  return { success: "User created!" };
 };
